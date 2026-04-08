8e4a8b78c4025b402ade6888a5f46b888849cdc8
SayanSarkar7 <sarkarsayan602@gmail.com>
2026-04-08 15:09:25 +0530

    update final commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Page</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+<link rel="stylesheet" href="/build/assets/app-8f8302ad.css">
+<script src="/build/assets/app-3720562b.js" defer></script>
 </head>
+
 
 <body>
     <x-user-navbar></x-user-navbar>
